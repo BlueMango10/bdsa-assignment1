782e89cdeae5d40d6753d5ef8a7cb1ae496d1857
Magnus Dalsgaard Larsen <daml@itu.dk>
2021-09-10 13:50:33 +0200

    Add tests

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -4,5 +4,45 @@ namespace Assignment1.Tests
 {
     public class IteratorsTests
     {
+        [Fact]
+        public void Flatten_returns_012345()
+        {
+            // Arrange
+            var stream = new[] {
+                new[]{0,1,2},
+                new[]{3,4},
+                new[]{5,6,7,8}
+            };
+
+            // Act
+            var result = Iterators.Flatten(stream);
+
+            // Assert
+            var expected = new[]{0,1,2,3,4,5,6,7,8}.GetEnumerator();
+            // TODO: Find the correct way to assert this.
+            foreach (var item in result)
+            {
+                expected.MoveNext();
+                Assert.Equal(result, expected.Current);
+            }
+        }
+
+        [Fact]
+        public void Filter_returns_246_from_1234567()
+        {
+            // Arrange
+            var input = new[]{1,2,3,4,5,6,7};
+
+            // Act
+            var result = Iterators.Filter(input, p => p % 2 == 0);
+
+            // Assert
+            var expected = new[]{2,3,6}.GetEnumerator();
+            foreach (var item in result)
+            {
+                expected.MoveNext();
+                Assert.Equal(result, expected.Current);
+            }
+        }
     }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -8,11 +8,17 @@ namespace Assignment1
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
             throw new NotImplementedException();
+            // For each stream
+            // For each element
+            // Yield return element
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
             throw new NotImplementedException();
+            // for each item in items
+            // if predicate(item)
+            //     yield return item
         }
     }
 }
