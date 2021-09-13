9f8cabbd3dd23e503f49c761de5685ec39582caa
Magnus Dalsgaard Larsen <45166031+BlueMango10@users.noreply.github.com>
2021-09-13 17:43:21 +0200

    Iterators (#2)
    * Add tests
    
    * Change test assert to what should be correct
    
    * Fix wrong assert order
    
    * Fix wrong expected value
    
    * Implement iterators
    
    Co-authored-by: Magnus Dalsgaard Larsen <daml@itu.dk>

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,8 +1,40 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace Assignment1.Tests
 {
     public class IteratorsTests
     {
+        [Fact]
+        public void Flatten_returns_012345()
+        {
+            // Arrange
+            var stream = new[] {
+                new[]{0,1,2},
+                new[]{3,4},
+                new[]{5,6,7,8}
+            };
+
+            // Act
+            var actual = Iterators.Flatten(stream);
+
+            // Assert
+            IEnumerable<int> expected = new[]{0,1,2,3,4,5,6,7,8};
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Filter_returns_246_from_1234567()
+        {
+            // Arrange
+            var input = new[]{1,2,3,4,5,6,7};
+
+            // Act
+            var actual = Iterators.Filter(input, p => p % 2 == 0);
+
+            // Assert
+            IEnumerable<int> expected = new[]{2,4,6};
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -7,12 +7,22 @@ namespace Assignment1
     {
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            throw new NotImplementedException();
+            foreach (var stream in items)
+            {
+                foreach (var item in stream)
+                {
+                    yield return item;
+                }
+            }
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
-            throw new NotImplementedException();
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                    yield return item;
+            }
         }
     }
 }
